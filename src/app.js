99b8dd78275f29c2a8fb2f9f50769188d03e3ae6
songxirong <1529120399@qq.com>
2018-07-14 10:27:54 +0800

    连接数据库完成注册登录功能

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
-// 使用express开启web服务
-var express = require('express');
-const path = require('path');
-var app = express();
-// node中处理静态资源
-app.use(express.static(path.join(__dirname,"statics")));
-// 集成路由中间件
-var accountRouter = require(path.join(__dirname,"./routers/accountRouter"));
-app.use('/',accountRouter);
+//1.0 导入express
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
+//2.0 创建app
+const app = express()
+
+//node中处理静态资源
+app.use(express.static(path.join(__dirname,"statics")))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(session({ secret: 'keyboard cat',resave:true,saveUninitialized:true, cookie: { maxAge: 10 * 60000 }}))
+
+//3.0 集成路由中间件【路由中间件写在所有中间件的后面】
+const accountRouter = require(path.join(__dirname,"./routers/accountRouter.js"))
+const studentManagerRouter = require(path.join(__dirname,"./routers/studentManagerRouter.js"))
+app.use('/account',accountRouter)
+app.use('/studentmanager',studentManagerRouter)
+
+//4.0 开启
 app.listen(3000,'127.0.0.1',err=>{
     if(err){
-        console.log(err);
+        console.log(err)
     }
-    console.log('start OK');
-});
+
+    console.log('start OK')
+})
--- src/routers/accountRouter.js
+++ src/routers/accountRouter.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const path = require('path');
-// 创建路由对象
-var accountRouter = express.Router();
-// 导入控制模块
-const accountControl = require(path.join(__dirname,"../controllers/accountControl.js"));
-// 发送get请求获取登录页面
-accountRouter.get('/login',accountControl.getLoginPage);
-// 发送get请求获取图片验证码
-accountRouter.get('/vcode',accountControl.getImageVcode);
+//导入express
+const express = require('express')
+const path = require('path')
 
-// 导出路由中间件
-module.exports = accountRouter;
+//创建路由对象
+// http://www.expressjs.com.cn/4x/api.html#router
+const accountRouter = express.Router()
+
+const accountCTRL = require(path.join(__dirname,"../controllers/accountController.js"))
+
+//当浏览器发送了 http://127.0.0.1:3000/account/login 交给对应的控制器 accountCTRL 的 getLoginPage 方法处理
+accountRouter.get('/login',accountCTRL.getLoginPage)
+
+//获取图片验证码
+accountRouter.get('/vcode',accountCTRL.getImageVcode)
+
+//获取注册页面
+accountRouter.get('/register',accountCTRL.getRegisterPage)
+
+//处理注册请求
+accountRouter.post('/register',accountCTRL.register)
+
+//处理登录请求
+accountRouter.post('/login',accountCTRL.login)
+
+//导出路由模块(路由中间件)
+module.exports = accountRouter
